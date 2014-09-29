490419d6acc0604ea6384e07929ccd1c26a6334e
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2014-09-29 14:12:21 +0200

    Initial commit

---
+++ src/main/java/tdd/movies/infrastructure/JpaMovieRepository.java
@@ -0,0 +1,28 @@
+package tdd.movies.infrastructure;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import tdd.movies.domain.Movie;
+import tdd.movies.domain.MovieRepository;
+
+public class JpaMovieRepository implements MovieRepository{
+
+    private EntityManager entityManager;
+
+    public JpaMovieRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Movie> loadAll() {
+        return entityManager.createQuery("from Movie").getResultList();
+    }
+
+    @Override
+    public void save(Movie movie) {
+        entityManager.persist(movie);
+    }
+
+}
